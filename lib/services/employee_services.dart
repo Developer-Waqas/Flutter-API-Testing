211d4de2be7c70ad438ec2aaa4b7aa214997c69f
Waqas Muhammad <waqas.developer5254@gmail.com>
2025-02-14 21:51:38 +0500

    first commit

---
+++ lib/services/employee_services.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:api_testing/models/employee_model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class EmployeeServices {
+  var baseUrl = 'https://reqres.in/api/';
+
+  getEmployeeData() async {
+    List<EmployeeModel> employeeList = [];
+    try {
+      var response = await http.get(Uri.parse(baseUrl + 'users?page=2'));
+      if (response.statusCode == 200) {
+        var data = response.body;
+        var decodeData = jsonDecode(data);
+        var employees = decodeData['data'];
+        for (var employee in employees) {
+          EmployeeModel newEmployee = EmployeeModel.fromJson(employee);
+          employeeList.add(newEmployee);
+        }
+        print(employeeList);
+        return employeeList;
+      }
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+}
---
+++ lib/views/home_screen.dart
@@ -0,0 +1,46 @@
+import 'package:api_testing/models/employee_model.dart';
+import 'package:api_testing/services/employee_services.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('API Testing'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: FutureBuilder(
+          future: EmployeeServices().getEmployeeData(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Something went wrong'),
+              );
+            } else if (snapshot.hasData) {
+              var data = snapshot.data as List<EmployeeModel>;
+
+              return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(data[index].avatar!),
+                      ),
+                      title: Text(data[index].firstName!),
+                      subtitle: Text(data[index].email!),
+                      trailing: Text(data[index].id.toString()),
+                    );
+                  });
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
+  }
+}
